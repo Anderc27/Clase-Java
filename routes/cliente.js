d1a14a4cf83af970d4d1a2bea6ff33b2d3492dee
Ander <andersonariascardona@gmail.com>
2025-03-13 19:38:48 -0500

    Segundo commit

--- routes/cliente.js
+++ routes/cliente.js
@@ -27,4 +27,57 @@ router.get('/', async (req, res) => {
     }
 });
 
+//obtener un solo cliente
+router.get('/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const [rows] = await pool.execute(
+            'SELECT * FROM cliente WHERE id_clientes =?',
+            [id]
+        );
+        if (rows.length > 0) {
+            res.json(rows[0]);
+        } else {
+            res.status(404).json({message: 'Cleinte no encontrado'}); 
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {nombre, email, telefono} = req.body;
+        const {results} = await pool.execute(
+            'UPDATE cliente SET Nombre=?, Email=?, Telefono=? WHERE id_clientes=?',
+            [nombre, email, telefono, id]
+        );
+        if (results.affectedRows > 0) {
+            res.json({id, nombre, email, telefono});
+        } else {
+            res.status(404).json({message: 'Cliente no encontrado'});
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {results} = await pool.execute(
+            'DELETE FROM cliente WHERE id_clientes=?',
+            [id]
+        );
+        if (results.affectedRows > 0) {
+            res.json({message: 'Cliente eliminado'});
+        } else {
+            res.status(404).json({message: 'Cliente no encontrado'});
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
 module.exports = router;
